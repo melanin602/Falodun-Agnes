a1913035f219eb847784f817ddddfe1440c284a3
Agnes <alababoluwatife51@gmail.com>
2025-11-08 13:45:02 +0100

    uses of operators

---
+++ index.js
@@ -0,0 +1,39 @@
+( arithmetic)
+console.log( 5+2)
+console.log(`ade`+`ola` )
+console.log( 10-2)
+console.log( 6*2)
+console.log( 8/2)
+console.log( 8%4)
+console.log( 2**3)
+
+
+let x=10( assignment)
+console.log(x=10 )
+console.log( x+=5)
+console.log( x-=6)
+console.log( x*=7)
+
+
+( comparison)
+console.log( `5`==5)
+console.log( 5==5)
+console.log( `5` === 5)
+console.log( 5 ===5)
+
+
+
+
+
+
+
+
+
+
+
+// conditional statements
+// let adultage= 18;
+
+// if (adultage > 18) {
+//     cconsole.log(you are
+// }
